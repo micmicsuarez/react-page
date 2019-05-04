e8da145b0342acd41ff706c5c77422368f3b69ad
Michael Nico Suarez <michaelnicosuarez@gmail.com>
2019-05-04 19:57:32 +0800

    Updates

---
+++ precache-manifest.977e15747b9b47f26d8eca24037c854d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a386cdae4813c1768df89c439a48af7d",
+    "url": "/react-page/index.html"
+  },
+  {
+    "revision": "eb3a459e81c852d842c7",
+    "url": "/react-page/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-page/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "eb3a459e81c852d842c7",
+    "url": "/react-page/static/js/main.e0dd71fc.chunk.js"
+  },
+  {
+    "revision": "d2b3f9b156d0a463a8af",
+    "url": "/react-page/static/js/runtime~main.a09b50d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
